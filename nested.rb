bcf9f3e3ac696169fb766785db54b738fbbab32c
SOULEYMANE SOW <souleymanesow@gmail.com>
2018-12-03 14:52:12 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+    
+    # programmer_hash[:dennis_ritchie][:languages].to_s
 end
 
 def adding_matz
@@ -65,7 +68,7 @@ def adding_matz
 # }
 
 	programmer_hash = 
- 		{
+ 		{ 
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -79,7 +82,8 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash {:yukihiro_matsumoto => :known_for}
+programmer_hash [:yukihiro_matsumoto][:languages]=["LISP", "C"]
     
 end
 
